6deb0245a539687bee123336fee3862d99b6e0a4
mahmoud <mahmoud.ghlive@gmail.com>
2018-10-12 16:18:06 +0300

    removed google api from environments.ts files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCGU_evaUmS40AHJuKYxs2XKmj42mDpOUw',
-    authDomain: 'kortasiyi-c26b4.firebaseapp.com',
-    databaseURL: 'https://kortasiyi-c26b4.firebaseio.com',
-    projectId: 'kortasiyi-c26b4',
-    storageBucket: 'kortasiyi-c26b4.appspot.com',
-    messagingSenderId: '582628133873'
+    apiKey: 'x',
+    authDomain: 'x',
+    databaseURL: 'x',
+    projectId: 'x',
+    storageBucket: 'x',
+    messagingSenderId: 'x'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCGU_evaUmS40AHJuKYxs2XKmj42mDpOUw',
-    authDomain: 'kortasiyi-c26b4.firebaseapp.com',
-    databaseURL: 'https://kortasiyi-c26b4.firebaseio.com',
-    projectId: 'kortasiyi-c26b4',
-    storageBucket: 'kortasiyi-c26b4.appspot.com',
-    messagingSenderId: '582628133873'
+    apiKey: 'x',
+    authDomain: 'x',
+    databaseURL: 'x',
+    projectId: 'x',
+    storageBucket: 'x',
+    messagingSenderId: 'x'
   }
 };
 
